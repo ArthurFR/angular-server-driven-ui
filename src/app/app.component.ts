776446cb2186dae2f17600b42644537af9b9db76
ArthurFR <contato.arthurfr@gmail.com>
2020-08-09 22:23:28 -0300

    Init project

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import 'node_modules/server-driven-ui/src';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-server-driven-ui';
+}
